694d6aafb6230ec1e043dc6f4069080b1acfbdd0
Ikuo Terado <eqobar@gmail.com>
2020-07-26 00:38:47 +0900

    Support drawing text

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,62 @@
 import Runtime from 'scratch-vm/src/engine/runtime'
 import ArgumentType from 'scratch-vm/src/extension-support/argument-type'
 import BlockType from 'scratch-vm/src/extension-support/block-type'
+import StageLayering from 'scratch-vm/src/engine/stage-layering'
 import Cast from 'scratch-vm/src/util/cast'
 
-class DummyExtension {
+class TextExtension {
   private runtime: Runtime
+
+  private penSkinId: any
+  private penDrawableId: any
+
+  private skin: any
+
+  private centerX: number
+  private centerY: number
+
+  private context: any
 
   constructor(runtime: Runtime) {
     this.runtime = runtime
+
+    this.runtime.on('targetWasCreated', () => {
+      this.createLayer()
+    })
+  }
+
+  private createLayer() {
+    if (!this.runtime.renderer) {
+      return
+    }
+
+    if (!this.penSkinId) {
+      this.penSkinId = this.runtime.renderer.createPenSkin()
+      this.penDrawableId = this.runtime.renderer.createDrawable(StageLayering.PEN_LAYER)
+      this.runtime.renderer.updateDrawableProperties(this.penDrawableId, {
+        skinId: this.penSkinId
+      })
+    }
+
+    if (!this.skin) {
+      this.skin = this.runtime.renderer._allSkins[this.penSkinId]
+
+      const skinSize = this.skin.size
+      this.centerX = skinSize[0] / 2
+      this.centerY = skinSize[1] / 2
+    }
+
+    if (!this.context) {
+      this.context = this.skin._canvas.getContext('2d')
+      this.context.font = '24px serif'
+      this.context.fillStyle = 'rgb(0,0,0)'
+    }
   }
 
   getInfo() {
     return {
-      id: 'dummy',
-      name: 'Dummy',
+      id: 'text',
+      name: 'Text',
       menuIconURI: require('../assets/images/menuIcon.png'),
       blockIconURI: require('../assets/images/blockIcon.png'),
       color1: '#a0a0a0',
@@ -22,13 +65,21 @@ class DummyExtension {
 
       blocks: [
         {
-          opcode: 'say',
-          blockType: BlockType.REPORTER,
-          text: 'say [MESSAGE]',
+          opcode: 'drawText',
+          blockType: BlockType.COMMAND,
+          text: 'draw [MESSAGE] at x: [X] y: [Y]',
           arguments: {
             MESSAGE: {
               type: ArgumentType.STRING,
               defaultValue: 'Hello, World!'
+            },
+            X: {
+              type: ArgumentType.STRING,
+              defaultValue: '0'
+            },
+            Y: {
+              type: ArgumentType.STRING,
+              defaultValue: '0'
             }
           }
         }
@@ -36,12 +87,17 @@ class DummyExtension {
     }
   }
 
-  say(args: any) {
+  drawText(args) {
     const message = Cast.toString(args.MESSAGE)
-    console.log(message)
+    const x = Cast.toNumber(args.X)
+    const y = Cast.toNumber(args.Y)
 
-    return message
+    this.context.fillText(message, this.centerX + x, this.centerY - y)
+
+    this.skin._canvasDirty = true
+    this.skin._silhouetteDirty = true
+    this.runtime.requestRedraw()
   }
 }
 
-export default DummyExtension
+export default TextExtension
